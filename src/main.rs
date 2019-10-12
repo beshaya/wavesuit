881c4b1629cb40e540822679a8a056ae5b0ad444
Benjamin Shaya <ben@welkinhealth.com>
2019-10-12 01:26:53 -0700

    Allow compilation on other platforms by supplying a fake led controller (mod display).

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,11 @@ use std::{error::Error, thread};
 use std::sync::{Mutex};
 use std::time::Duration;
 
-use blinkt::Blinkt;
-
 use signal_hook::{iterator::Signals, SIGINT, SIGTERM};
 
 use crossbeam_channel::{bounded, tick, Receiver, select};
 
+mod display;
 mod painter;
 
 #[get("/"), format = "json"]
@@ -62,12 +61,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let webserver = rocket_channel(params.clone())?;
     let ticks = tick(Duration::from_millis(30));
 
-    // Remember to enable spi via raspi-config!
-    let mut blinkt = Blinkt::with_spi(16_000_000, 144)?;
 
     let width: usize = 4;
     let height: usize = 30;
     let dots: usize = width * height;
+
+    // Remember to enable spi via raspi-config!
+    let mut display = display::new(dots);
+
     let mut arm_painter = painter::make_painter("hex", width, height, params);
 
     loop {
@@ -76,9 +77,9 @@ fn main() -> Result<(), Box<dyn Error>> {
                 arm_painter.paint();
                 for i in 0..dots {
                     let pixel = arm_painter.get(i);
-                    blinkt.set_pixel(i, pixel.r, pixel.g, pixel.b);
+                    display.set_pixel(i, pixel.r, pixel.g, pixel.b);
                 }
-                blinkt.show()?;
+                display.show()?;
             }
             recv(ctrl_c_events) -> _ => {
                 println!();
